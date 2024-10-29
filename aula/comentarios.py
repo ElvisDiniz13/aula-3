9d3b803ba2af4fb05b6fda36bf37eed9b38eb23d
ElvisDiniz13 <elvisdiniz13@hotmail.com>
2024-10-28 21:10:58 -0300

    comentariosPyCriado

---
+++ aula/comentarios.py
@@ -0,0 +1,16 @@
+# comentário de uma linha
+# código que exibe uma mensagem no terminal
+print('Uma mensagem')
+
+'''
+Este é um comentário 
+em mais de uma linha,
+que será ignorado pelo interpretador do Python.
+'''
+
+"""
+Assim
+também 
+funciona em 
+muitas linhas.
+"""
--- aula/primeiro_programa.py
+++ aula/primeiro_programa.py
@@ -1,1 +1,4 @@
 print('Hello World')
+#nome = 'Elvis'
+nome = input("Qual é o seu nome?\n")
+print(f'Oi {nome}, você está bem?')
