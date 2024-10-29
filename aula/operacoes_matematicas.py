d7980f7f4580a7436bb8c5f06c37ef83dfec0a14
ElvisDiniz13 <elvisdiniz13@hotmail.com>
2024-10-28 21:39:32 -0300

    addPyFiles

---
+++ aula/operacoes_matematicas.py
@@ -0,0 +1,18 @@
+#print('1 + 1 = ',1+1)
+#soma +
+print(f'1 + 1 = {1+1}')
+
+#subtração -
+print(f'3 - 1 = {3-1}')
+
+#multiplicação *
+print(f'5 x 6 = {5*6}')
+
+#divisão /
+print(f'10 / 3 = {10/3}')
+
+#exponenciação **
+print(f'2³ = {2**3}')
+
+#modulo (resto %)
+print(f'10 % 4 = {10%4}')
---
+++ aula/tipos.py
@@ -0,0 +1,14 @@
+print('Tipo int:', 1, "=>", type(1))
+
+idade = '35'
+print(type(idade))
+idade = int(idade)
+print(type(idade))
+
+print('Tipo float:', 42.33, "=>", type(42.33))
+
+print('Tipo srt:', "string", "=>", type("string"))
+
+print('Tipo bool:', True, False, "=>", type(True))
+
+print('Tipo list:', [1, 2, 3, 4, 5], "=>", type([1, 2, 3, 4, 5]))
